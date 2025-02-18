bd093671af14fd069a9a4c8eb079dbd1ba02c601
Matthieu Fouquet <mfouquet@sqli.com>
2025-02-18 11:03:52 +0100

    add output api get url

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "api_fqdn" {
   value = azurerm_container_app.api.ingress[0].fqdn
 }
+
+output "api_get_product_list_url" {
+  value = "${azurerm_container_app.api.ingress[0].fqdn}/api/Product/productlist"
+}
+
